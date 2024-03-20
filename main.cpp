ebb96f3441e3d9d9ad1498b11ab872526edf5d80
Kazuhiro <91428298+KasuhiroSaitashi@users.noreply.github.com>
2024-03-20 11:49:59 +0100

    Add console refreshing and check what system player use

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <cstdlib>
 #include "Item.h"
 #include "Player.h"
 #include "Storage.h"
@@ -8,13 +9,24 @@
 
 using namespace std;
 
+#ifdef _WIN32
+const std::string os_name = "Windows";
+#elif __APPLE__
+const std::string os_name = "Mac OS";
+#elif __linux__
+const std::string os_name = "Linux";
+#else
+const std::string os_name = "Unknown";
+#endif
 
 int main()
 {
     Player P;
     Game game(P);
     //P.showEq();
-    while(true){
+    while (true){
+		if (os_name == "Windows") system("cls");
+		else system("clear"); 
 		P.DoIHaveSomeFreeSpace();
         game.showUI();
         game.input();
